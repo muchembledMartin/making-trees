30ea7a52abf07f809ff209a1f106dced1bdc7b4e
--replace-all <muchembled.martin@gmail.com>
2024-06-14 16:31:12 +0200

    Minimal setup for the project

---
+++ project/CMakeFiles/DH2323SkeletonSDL2.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/martin/Documents/cours/BA6/making-trees/SDL2Auxiliary/SDL2Auxiliary.cpp" "CMakeFiles/DH2323SkeletonSDL2.dir/SDL2Auxiliary/SDL2Auxiliary.cpp.o" "gcc" "CMakeFiles/DH2323SkeletonSDL2.dir/SDL2Auxiliary/SDL2Auxiliary.cpp.o.d"
+  "/home/martin/Documents/cours/BA6/making-trees/SkeletonSDL2.cpp" "CMakeFiles/DH2323SkeletonSDL2.dir/SkeletonSDL2.cpp.o" "gcc" "CMakeFiles/DH2323SkeletonSDL2.dir/SkeletonSDL2.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project/CMakeFiles/DH2323SkeletonSDL2.dir/SkeletonSDL2.cpp.o.d
@@ -0,0 +1,235 @@
+CMakeFiles/DH2323SkeletonSDL2.dir/SkeletonSDL2.cpp.o: \
+ /home/martin/Documents/cours/BA6/making-trees/SkeletonSDL2.cpp \
+ /usr/include/stdc-predef.h \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/glm.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/core/_fixes.hpp \
+ /usr/include/c++/7/cmath \
+ /usr/include/x86_64-linux-gnu/c++/7/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/7/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/7/bits/cpu_defines.h \
+ /usr/include/c++/7/bits/cpp_type_traits.h \
+ /usr/include/c++/7/ext/type_traits.h /usr/include/math.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/math-vector.h \
+ /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-narrow.h \
+ /usr/include/x86_64-linux-gnu/bits/iscanonical.h \
+ /usr/include/c++/7/bits/std_abs.h /usr/include/stdlib.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endianness.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/bits/select2.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/x86_64-linux-gnu/bits/atomic_wide_counter.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_mutex.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-bsearch.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib.h /usr/include/c++/7/climits \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/pthread_stack_min-dynamic.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/xopen_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/uio_lim.h /usr/include/c++/7/cfloat \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/float.h \
+ /usr/include/c++/7/limits /usr/include/c++/7/cstdio /usr/include/stdio.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/cookie_io_functions_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio2.h \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/core/setup.hpp \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/emmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/xmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/mmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/mm_malloc.h \
+ /usr/include/c++/7/stdlib.h /usr/include/c++/7/cstdlib \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/_detail.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/setup.hpp \
+ /usr/include/c++/7/cassert /usr/include/assert.h \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_half.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_half.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/_detail.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_float.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_int.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_gentype.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_size.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_vec1.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_vec.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/_swizzle.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/_swizzle_func.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_vec1.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_vec2.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_vec2.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_vec3.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_vec3.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_vec4.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_vec4.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat2x2.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat2x2.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat2x3.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat2x3.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat2x4.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat2x4.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat3x2.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat3x2.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat3x3.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat3x3.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat3x4.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat3x4.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat4x2.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat4x2.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat4x3.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat4x3.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat4x4.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/type_mat4x4.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_trigonometric.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_trigonometric.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/_vectorize.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_exponential.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_exponential.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_common.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/_fixes.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_common.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_packing.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_packing.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_geometric.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_geometric.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_matrix.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_matrix.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_vector_relational.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_vector_relational.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_integer.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_integer.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_noise.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/func_noise.inl \
+ /home/martin/Documents/cours/BA6/making-trees/glm/glm/./core/_swizzle.hpp \
+ /home/martin/Documents/cours/BA6/making-trees/SDL2Auxiliary/SDL2Auxiliary.h \
+ /usr/local/include/SDL2/SDL.h /usr/local/include/SDL2/SDL_main.h \
+ /usr/local/include/SDL2/SDL_stdinc.h \
+ /usr/local/include/SDL2/SDL_config.h \
+ /usr/local/include/SDL2/SDL_platform.h \
+ /usr/local/include/SDL2/begin_code.h \
+ /usr/local/include/SDL2/close_code.h /usr/include/string.h \
+ /usr/include/strings.h \
+ /usr/include/x86_64-linux-gnu/bits/strings_fortified.h \
+ /usr/include/x86_64-linux-gnu/bits/string_fortified.h \
+ /usr/include/wchar.h /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/types/wint_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar2.h /usr/include/inttypes.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h /usr/include/ctype.h \
+ /usr/include/c++/7/math.h /usr/local/include/SDL2/SDL_assert.h \
+ /usr/local/include/SDL2/SDL_atomic.h /usr/local/include/SDL2/SDL_audio.h \
+ /usr/local/include/SDL2/SDL_error.h /usr/local/include/SDL2/SDL_endian.h \
+ /usr/local/include/SDL2/SDL_mutex.h /usr/local/include/SDL2/SDL_thread.h \
+ /usr/local/include/SDL2/SDL_rwops.h \
+ /usr/local/include/SDL2/SDL_clipboard.h \
+ /usr/local/include/SDL2/SDL_cpuinfo.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/immintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/pmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/tmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/smmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/popcntintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/wmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avxintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx2intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512fintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512erintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512pfintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512cdintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512vlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512bwintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512dqintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512vlbwintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512vldqintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512ifmaintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512ifmavlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512vbmiintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512vbmivlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx5124fmapsintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx5124vnniwintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512vpopcntdqintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/shaintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/lzcntintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/bmiintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/bmi2intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/fmaintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/f16cintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/rtmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/xtestintrin.h \
+ /usr/local/include/SDL2/SDL_events.h /usr/local/include/SDL2/SDL_video.h \
+ /usr/local/include/SDL2/SDL_pixels.h /usr/local/include/SDL2/SDL_rect.h \
+ /usr/local/include/SDL2/SDL_surface.h \
+ /usr/local/include/SDL2/SDL_blendmode.h \
+ /usr/local/include/SDL2/SDL_keyboard.h \
+ /usr/local/include/SDL2/SDL_keycode.h \
+ /usr/local/include/SDL2/SDL_scancode.h \
+ /usr/local/include/SDL2/SDL_mouse.h \
+ /usr/local/include/SDL2/SDL_joystick.h \
+ /usr/local/include/SDL2/SDL_guid.h \
+ /usr/local/include/SDL2/SDL_gamecontroller.h \
+ /usr/local/include/SDL2/SDL_sensor.h /usr/local/include/SDL2/SDL_quit.h \
+ /usr/local/include/SDL2/SDL_gesture.h \
+ /usr/local/include/SDL2/SDL_touch.h \
+ /usr/local/include/SDL2/SDL_filesystem.h \
+ /usr/local/include/SDL2/SDL_haptic.h \
+ /usr/local/include/SDL2/SDL_hidapi.h /usr/local/include/SDL2/SDL_hints.h \
+ /usr/local/include/SDL2/SDL_loadso.h /usr/local/include/SDL2/SDL_log.h \
+ /usr/local/include/SDL2/SDL_messagebox.h \
+ /usr/local/include/SDL2/SDL_metal.h /usr/local/include/SDL2/SDL_power.h \
+ /usr/local/include/SDL2/SDL_render.h /usr/local/include/SDL2/SDL_shape.h \
+ /usr/local/include/SDL2/SDL_system.h /usr/local/include/SDL2/SDL_timer.h \
+ /usr/local/include/SDL2/SDL_version.h \
+ /usr/local/include/SDL2/SDL_locale.h /usr/local/include/SDL2/SDL_misc.h \
+ /home/martin/Documents/cours/BA6/making-trees/rasterizer/Utils.h
